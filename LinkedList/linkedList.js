0fe19afb7ac698dfd59e4d8bfef6ec965d6755c0
二零九八 <huanghuiqiang86@gmail.com>
2021-04-30 21:13:02 +0800

    add: rever single linkedlist

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -60,7 +60,25 @@ link.insertAtTail(3);
 link.insertAtTail(4);
 link.insertAtTail(5);
 
+
+let head = link.getAtIndex(1);
+
+console.log(head);
+
+// 利用递归反转链表
+function reverse(head) {
+  // base case
+  if (head.next === null) return head;
+  // 递归的方法
+  // 输入一个head节点，将以head为起点的链表反转，返回反转之后的头节点last
+  let last = reverse(head.next);
+  head.next.next = head;
+  head.next = null;
+  return last;
+};
+
+
 // link.deleteAtIndex(5)
 
 
-console.log(link.getAtIndex(2));
+console.log(reverse(head));
