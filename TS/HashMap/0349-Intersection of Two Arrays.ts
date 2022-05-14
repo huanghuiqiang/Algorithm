a46e7a84175ddc13dff3a4f00d792deaa485d865
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-05-14 19:56:06 +0800

    feat: add 0202 0349

---
+++ TS/HashMap/0349-Intersection of Two Arrays.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: huanghq 
+ * @Date: 2022-05-13 18:24:23 
+ * @Last Modified by: huanghq
+ * @Last Modified time: 2022-05-14 19:55:46
+ */
+
+function intersection(num1: number[], nums2: number[]): number[] {
+  let resSet: Set<number> = new Set(),
+      nums1Set: Set<number> = new Set(num1);
+
+  return 
+}
