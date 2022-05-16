bd65ddab59a5303bac0d9b28db13e804283c2935
Hugh 2098 <huanghuiqiang86@gmail.com>
2022-05-16 18:17:52 +0800

    feat: ransom

---
+++ TS/HashMap/0383-Ransom Note.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: huanghq 
+ * @Date: 2022-05-16 15:50:10 
+ * @Last Modified by: huanghq
+ * @Last Modified time: 2022-05-16 18:00:35
+ */
+
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  let helperArr: number[] = new Array(26).fill(0);
+  let base: number = 'a'.charCodeAt(0);
+  let index: number;
+  for (let i = 0, length = magazine.length; i < length; i++) {
+    helperArr[magazine[i].charCodeAt(0) - base]++;
+  }
+  for (let i = 0, length = ransomNote.length; i < length; i++) {
+    index = ransomNote[i].charCodeAt(0) - base;
+    helperArr[index]--;
+    if (helperArr[index] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
